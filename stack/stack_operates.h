b4218fbdaa427f5723becde152ffa5db1056f8f9
wangweilong <wwbensky@gmail.com>
2013-11-18 13:47:09 +0800

    create stack operates

---
+++ stack/stack_operates.h
@@ -0,0 +1,18 @@
+#ifndef VA_STACK_STACK_OPERATES_H_
+#define VA_STACK_STACK_OPERATES_H_
+
+class StackOperates
+{
+    private:
+        unsigned int &stack_buffer[1000];
+         
+    public:
+        StackOperates();
+        unsigned int (&create_buffer())[];
+
+        push
+
+        void print();
+}
+
+#endif
