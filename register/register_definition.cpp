d5247811b345a662b12a0714426c0efe5bf854e9
wangweilong <wwbensky@gmail.com>
2013-11-21 16:08:50 +0800

    change how to init the register

--- register/register_definition.cpp
+++ register/register_definition.cpp
@@ -1,7 +1,6 @@
 #include "register_definition.h"
 
-RegisterDefinition::RegisterDefinition():
-    register_definition(create_register_definition())
+RegisterDefinition::RegisterDefinition()
 {
 }
 
@@ -13,67 +12,58 @@ int RegisterDefinition::get_binary(std::string key_register)
 }
 
 
-std::map<std::string, int>& RegisterDefinition::
-create_register_definition()
-{
-    static std::map<std::string, int>m;
-    static int mutex = 0;
-    if(mutex == 0)
-    {
-        mutex = 1;
-        m["rax"] = 00000;
-        m["raxll"] = 00001;
-        m["raxlh"] = 00002;
-        m["raxhl"] = 00003;
-        m["raxhh"] = 00004;
+std::map<std::string, int> RegisterDefinition::\
+        register_definition = {
+        {"rax", 0x00},
+        {"raxll", 0x01},
+        {"raxlh", 0x02},
+        {"raxhl", 0x03},
+        {"raxhh", 0x04},
 
-        m["rbx"] = 00010;
-        m["rbxll"] = 00011;
-        m["rbxlh"] = 00012;
-        m["rbxhl"] = 00013;
-        m["rbxhh"] = 00014;
+        {"rbx", 0x10},
+        {"rbxll", 0x11},
+        {"rbxlh", 0x12},
+        {"rbxhl", 0x13},
+        {"rbxhh", 0x14},
 
-        m["rcx"] = 00020;
-        m["rcxll"] = 00021;
-        m["rcxlh"] = 00022;
-        m["rcxhl"] = 00023;
-        m["rcxhh"] = 00024;
+        {"rcx", 0x20},
+        {"rcxll", 0x21},
+        {"rcxlh", 0x22},
+        {"rcxhl", 0x23},
+        {"rcxhh", 0x24},
 
-        m["rdx"] = 00030;
-        m["rdxll"] = 00031;
-        m["rdxlh"] = 00032;
-        m["rdxhl"] = 00033;
-        m["rdxhh"] = 00034;
+        {"rdx", 0x30},
+        {"rdxll", 0x31},
+        {"rdxlh", 0x32},
+        {"rdxhl", 0x33},
+        {"rdxhh", 0x34},
 
-        m["rsp"] = 00040;
-        m["rspll"] = 00041;
-        m["rsplh"] = 00042;
-        m["rsphl"] = 00043;
-        m["rsphh"] = 00044;
+        {"rsp", 0x40},
+        {"rspll", 0x41},
+        {"rsplh", 0x42},
+        {"rsphl", 0x43},
+        {"rsphh", 0x44},
 
-        m["rdp"] = 00050;
-        m["rdpll"] = 00051;
-        m["rdplh"] = 00052;
-        m["rdphl"] = 00053;
-        m["rdphh"] = 00054;
+        {"rdp", 0x50},
+        {"rdpll", 0x51},
+        {"rdplh", 0x52},
+        {"rdphl", 0x53},
+        {"rdphh", 0x54},
 
-        m["rsi"] = 00060;
-        m["rsill"] = 00061;
-        m["rsilh"] = 00062;
-        m["rsihl"] = 00063;
-        m["rsihh"] = 00064;
+        {"rsi", 0x60},
+        {"rsill", 0x61},
+        {"rsilh", 0x62},
+        {"rsihl", 0x63},
+        {"rsihh", 0x64},
 
-        m["rdi"] = 00070;
-        m["rdill"] = 00071;
-        m["rdilh"] = 00072;
-        m["rdihl"] = 00073;
-        m["rdihh"] = 00074;
+        {"rdi", 0x70},
+        {"rdill", 0x71},
+        {"rdilh", 0x72},
+        {"rdihl", 0x73},
+        {"rdihh", 0x74},
 
-        m["zf"] = 00100;
-        m["sf"] = 00101;
-        m["of"] = 00102;
-        m["cf"] = 00103;
-        m["pc"] = 00104;
-    }
-    return m;
-}
+        {"zf", 0x80},
+        {"sf", 0x81},
+        {"of", 0x82},
+        {"cf", 0x83},
+        {"pc", 0x84}};
