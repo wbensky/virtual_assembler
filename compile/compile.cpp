6e4ad50e108a29821e01349f10e80db42a924beb
wangweilong <wwbensky@gmail.com>
2013-11-28 17:14:20 +0800

    change get int function and add operate text

--- compile/compile.cpp
+++ compile/compile.cpp
@@ -1,5 +1,21 @@
 #include"compile.h"
 
+std::set<std::string> CompileManage::jmp_instructions ={
+    "jmp",
+    "je",
+    "jne",
+    "js",
+    "jns",
+    "jg",
+    "jge",
+    "jl",
+    "jle",
+    "ja",
+    "jae",
+    "jb",
+    "jbe"
+};
+
 CompileManage::CompileManage()
 {
 }
@@ -32,13 +48,6 @@ void CompileManage::get_labels()
                 labels_record[code_file[i].substr(0, n)] = -1;
         }
     }
-    //std::cout << labels_record.size()<< std::endl;
-    //std::cout << error_record.size()<<std::endl;
-    //std::map<std::string, int>::iterator it = labels_record.begin();
-    //for(; it != labels_record.end(); ++it)
-    //{
-    //   std::cout << it->first <<": " << it->second<<std::endl;
-    //}
 }
 
 bool CompileManage::is_vars(std::string var, std::string &error_temp)
@@ -108,40 +117,104 @@ void CompileManage::operate_code_file()
             i = get_data(i);
             continue;
         }
+
         n = temp.find(".usect");
         if(n !=  std::string::npos)
         {
             i = get_usect(i);
             continue ;
         }
+
+        n = temp.find(".text");
+        if(n != std::string::npos)
+        {
+            i = get_text(i);
+            continue;
+        }
     }
 }
 
-void CompileManage::get_int_from_usect(int)
+int CompileManage::get_int_from_string(std::string str,int &num_end)
 {
-    
+    int len_str = str.size();
+    int flag = 0;
+    int start;
+    int end;
+    for (start= 0 ; start< len_str && (str[start] < '0' ||str[start] > '9' ); start++)
+    {
+        if(str[start] == '-')
+        {
+            flag = 1;
+            break;
+        }
+    }
+    for(;start < len_str && (str[start] < '0' || str[start] > '9'); start++)
+        ;
+    for(end = start; end < len_str &&str[end] <= '9' && str[end] >= '0'; end ++)
+        ;
+    num_end = end;
+    if(flag == 1)
+        return -std::stoi(str.substr(start, end -start));
+    num_end = end;
+    return std::stoi(str.substr(start, end -start));
 }
 
-void CompileManage::get_uint_from_usect(int)
+unsigned long  CompileManage::get_uint_from_string(std::string str, int& num_end)
 {
+    int len_str = str.size();
+    int flag = 0;
+    int start;
+    int end;
+
+    for( start = 0;start < len_str && (str[start] < '0' || str[start] > '9'); start++)
+        ;
+    for(end = start; end < len_str &&str[end] <= '9' && str[end] >= '0'; end ++)
+        ;
+    num_end = end;
+    return std::stoul(str.substr(start, end -start));
 }
+
+void CompileManage::get_int_from_usect(int pos)
+{
+    int temp;
+    change_var(get_var(pos));
+
+    std::string temp_str = code_file[pos];
+    std::string::size_type n = temp_str.find(',');
+    temp = get_int_from_string(temp_str.substr(n + 1, temp_str.size()),temp);
+    for(int i = 0 ;i < temp; i++)
+        write_into_binary_file(0);
+}
+
+void CompileManage::get_uint_from_usect(int pos)
+{
+    int temp;
+    change_var(get_var(pos));
+
+    std::string temp_str = code_file[pos];
+    std::string::size_type n = temp_str.find(',');
+    temp = get_int_from_string(temp_str.substr(n + 1, temp_str.size()),temp);
+    for(int i = 0 ;i < temp; i++)
+        write_into_binary_file(1);
+}
+
 std::string CompileManage::get_var(int pos)
 {
     int i, j, k;
-    std::string::size_type n;  
+    std::string::size_type n;
     std::string temp = code_file[pos];
-        n = temp.find(",");
-        if(n == std::string::npos)
-        {
-            for( j = 0; j < temp.size(); j ++)
-                if(temp[j] != ' ' && temp[j] != '\t')
-                    error_record.push_back("syntax error: var error at line" + std::to_string(i));
-            return ""; 
-        }
-        for(k = n -1; temp[k] == ' '||temp[k] == '\t'; k--)
-            ;
-        for(j=k ; temp[j] != ' ' && temp[j] != '\t';j-- )
-            ;
+    n = temp.find(",");
+    if(n == std::string::npos)
+    {
+        for( j = 0; j < temp.size(); j ++)
+            if(temp[j] != ' ' && temp[j] != '\t')
+                error_record.push_back("syntax error: var error at line" + std::to_string(i));
+        return ""; 
+    }
+    for(k = n -1; temp[k] == ' '||temp[k] == '\t'; k--)
+        ;
+    for(j=k ; temp[j] != ' ' && temp[j] != '\t';j-- )
+        ;
 
     return temp.substr(j + 1, k-j);
 }
@@ -152,6 +225,7 @@ void CompileManage::get_vars(int pos)
     std::string::size_type pos_start;
     std::string::size_type pos_end;
     std::string error_str;
+
     for(i = pos + 1;
             code_file[i].find(".end_usect") == std::string::npos;
             i++)
@@ -197,6 +271,152 @@ int CompileManage::get_usect(int pos)
     return i;
 }
 
+
+
+int CompileManage::get_text(int pos)
+{
+    int i;
+    std::string::size_type n;
+    for(i = pos + 1;
+            code_file[i].find(".end") ==std::string::npos;
+            i++)
+    {
+        InstructionStructure temp;
+        if(string_is_empty(code_file[i]))
+            continue;
+        temp.label = get_label(code_file[i]);
+        temp.instruction = get_instruction(code_file[i]);
+        temp.arg1 = get_arg1(code_file[i]);
+        temp.arg2 = get_arg2(code_file[i]);
+        instructions_record.push_back(temp);
+    }
+    add_nop_conflict();
+    add_nop_jmp();
+    return i;
+}
+
+void CompileManage::add_nop_conflict()
+{
+
+    InstructionStructure temp;
+    temp.instruction = "nop";
+    for(auto it = instructions_record.begin(); it != instructions_record.end(); it++)
+    {
+        auto next = ++it;
+        it--;
+        if(next == instructions_record.end())
+            break;
+        if(it -> arg1!="" && next ->arg1 != ""&&next->arg2 != "")
+            if(it->arg1 == next->arg1 || it->arg1 == next->arg2)
+            {    
+                instructions_record.insert(next,temp);
+                continue; 
+            }
+         if(it -> arg2!="" && next ->arg1 != ""&&next->arg2 != "")
+            if(it->arg2 == next->arg1 || it->arg2 == next->arg2)
+            { 
+                instructions_record.insert(next,temp);
+                continue; 
+            } 
+    
+    }
+
+}
+
+void CompileManage::add_nop_jmp()
+{
+    for(auto it = instructions_record.begin(); it != instructions_record.end(); it ++)
+        if(jmp_instructions.find(it -> instruction)!= jmp_instructions.end())
+        {
+            InstructionStructure temp;
+            it ++;
+            temp.instruction = "nop";
+            instructions_record.insert(it, 2, temp);
+            it --;
+        }
+}
+
+std::string CompileManage::get_arg1(std::string str)
+{
+    std::string::size_type n = str.find(":");
+    int end;
+    int start;
+
+    if(n == std::string::npos)
+        start = 0;
+    else
+        start = n + 1;
+    for(; str[start] == ' ' || str[start] == '\t'; start++ )
+        ;
+    for(; start < str.size()&&str[start] >= 'a' && str[start] <= 'z'; start++ )
+        ;
+    if(start == str.size())
+        return "";
+    for(;str[start] ==' ' || str[start] == '\t'; start++)
+        ;
+    if(start == str.size())
+        return "";
+
+    for(end = start; end < str.size()&&str[end] >= 'a' && str[end] <= 'z' ; end++)
+        ;
+    return str.substr(start, end - start);
+}
+
+std::string CompileManage::get_arg2(std::string str)
+{
+    int start, end;
+    std::string::size_type n = str.find(",");
+    if(n == std::string::npos)
+        return "";
+    start = n + 1;
+    for(; str[start]== ' ' || str[start] == '\t'; start++)
+        ;
+
+    for(end = start; end < str.size() && str[end] >='a' && str[end] <='z'; end++)
+        ;
+
+    return str.substr(start, end - start);
+}
+
+std::string CompileManage::get_instruction(std::string str)
+{
+    std::string::size_type n= str.find(":");
+    int start = 0;
+    int end;
+
+    if (n != std::string::npos)
+        start = n + 1;
+    for(; str[start] ==' ' || str[start] == '\t'; start ++)
+        ;
+    for(end = start; str[end] <= 'z' && str[end] >= 'a'; end ++)
+        ;
+
+    return str.substr(start, end - start);
+}
+
+
+std::string CompileManage::get_label(std::string str)
+{
+    int end;
+    std::string::size_type n = str.find(":");
+
+    if(n == std::string::npos)
+        return "";
+    for( end = n -1; str[end] == ' ' || str[end] == '\t'; end--)
+        ;
+
+    return str.substr(0, end + 1);
+}
+
+bool CompileManage::string_is_empty(std::string str)
+{
+    for(auto item: str)
+        if( !(item == ' '|| item == '\t'))
+            return false;
+
+    return true;
+}
+
 int CompileManage::get_data(int pos)
 {
     int i;
@@ -216,9 +436,10 @@ int CompileManage::get_data(int pos)
             continue;
         }
     }
-
     return i;
 }
+
+
 
 void CompileManage::write_into_binary_file(int num)
 {
@@ -246,17 +467,9 @@ void CompileManage::get_uint_from_data(int row)
 
     for(int i = n + 5; ; i++)
     {
-        int num_start=0;
         int num_end = 0;
-        for(; temp[num_start] <'0' || temp[num_start] > '9';
-                num_start++ )
-            ;
-        for(num_end = num_start;
-                temp[num_end] <= '9' && temp[num_end] >= '0';
-                num_end++ )
-            ;
 
-        write_into_binary_file( std::stoul(temp.substr(num_start,num_end - num_start)) );
+        write_into_binary_file(get_uint_from_string(temp, num_end));
         for(; num_end != temp.size() &&
                 !(temp[num_end] <='9' && temp[num_end] >= '0');
                 num_end++ )
@@ -271,6 +484,7 @@ void CompileManage::get_int_from_data(int row)
 {
     std::string temp = code_file[row];
     std::string::size_type n = code_file[row].find(".int");
+
     if(!(temp[n + 4] == ' ' || temp[n + 4] == '\t'))
     {
         error_record.push_back("syntax error: is .int? at line: " + std::to_string(row));
@@ -278,34 +492,16 @@ void CompileManage::get_int_from_data(int row)
     temp = temp.substr(n + 4);
     for(int i = n + 4; ; i++)
     {
-        int num_start=0;
         int num_end = 0;
-        int flag = 0;
-        for(; temp[num_start] <'0' || temp[num_start] > '9';
-                num_start++ )
-        {
-            if(temp[num_start]== '-')
-            {
-                num_start += 1;
-                flag = 1;
-                break;
-            }
-        }
-        for(num_end = num_start;
-                temp[num_end] <= '9' && temp[num_end] >= '0';num_end++ )
-            ;
-
-        if(flag == 1)
-            write_into_binary_file( -std::stoi(temp.substr(num_start,num_end - num_start)) );
-        else
-            write_into_binary_file( std::stoi(temp.substr(num_start,num_end - num_start)) );
+        write_into_binary_file(get_int_from_string(temp, num_end));
         for(; num_end != temp.size() &&
-                !(temp[num_end] <='9' && temp[num_end] >= '0');
+                !(temp[num_end] <='9' && temp[num_end] >= '0')&&
+                temp[num_end]!='-';
                 num_end++ )
             ;
         if(num_end == temp.size())
             break;
-        temp = temp.substr(num_end, temp.size());
+        temp = temp.substr(num_end, temp.size() );
     }
 }
 
@@ -314,34 +510,50 @@ void CompileManage::change_label(int row, int col)
     labels_record[code_file[row].substr(0, col)]  = binary_file.size();
 }
 
+void CompileManage::change_var(std::string str)
+{
+    vars_record[str] = binary_file.size();
+}
+
 void CompileManage::print_all()
 {
-    print_labels_record();
-    print_vars_record();
+    //print_labels_record();
+    //print_vars_record();
     //print_error_record();
     //print_code_file();
-    print_binary_file();
+    //print_binary_file();
+    print_instructions_record();
+}
+
+void CompileManage::print_instructions_record()
+{
+    std::cout << "instructions record:"<<std::endl;
+    for(auto it: instructions_record)
+        std::cout << "l:" + it.label +"\t" +
+            it.instruction + "\ta1:" +
+            it.arg1  +  "\ta2:" + it.arg2 <<std::endl;
+    std::cout << std::endl;
 }
 
 void CompileManage::print_vars_record()
 {
-    std::cout << "vars_record" <<std::endl;
-    std::map<std::string, int>::iterator it = vars_record.begin();
+    std::cout << "vars_record:" <<std::endl;
 
-    for(;it != vars_record.end(); it++)
+    for(auto it: vars_record)
     {
-        std::cout << it->first <<":\t" << it->second <<"\n";
+        std::cout << it.first <<":\t" << it.second <<"\n";
     }
+    std::cout<<std::endl;
 }
 
 void CompileManage::print_labels_record()
 {
-    std::cout << "labels_record" <<std::endl;
-    std::map<std::string, int>::iterator it = labels_record.begin();
-    for(;it != labels_record.end(); it++)
+    std::cout << "labels_record;" <<std::endl;
+    for(auto it = labels_record.begin();it != labels_record.end(); it++)
     {
         std::cout << it->first <<":\t" << it->second <<"\n";
     }
+    std::cout <<std::endl;
 }
 
 void CompileManage::print_binary_file()
@@ -363,6 +575,7 @@ void CompileManage::print_code_file()
         std::cout << code_file[i] << std::endl;
     }
 }
+
 void CompileManage::print_error_record()
 {
     std::cout << "error_file:" <<std::endl;
@@ -371,3 +584,4 @@ void CompileManage::print_error_record()
         std::cout << error_record[i] << std::endl;
     }
 }
+
--- compile/compile.h
+++ compile/compile.h
@@ -6,6 +6,17 @@
 #include<map>
 #include<fstream>
 #include<iostream>
+#include<set>
+#include<list>
+
+class InstructionStructure
+{
+    public:
+        std::string label;
+        std::string instruction;
+        std::string arg1;
+        std::string arg2;
+};
 
 class CompileManage
 {
@@ -15,15 +26,23 @@ class CompileManage
         std::map<std::string, int> labels_record;
         std::vector <std::string> error_record;
         std::map <std::string, int> vars_record;
+        //about instruction
 
+        static std::set<std::string> jmp_instructions;
+        std::list<InstructionStructure> instructions_record; 
     public:
         CompileManage();
+
+        //utils
+        bool string_is_empty(std::string);
+
         void read_code_from_file(std::string );
         void get_labels();
         void change_label(int, int );
         bool is_labels(std::string ,std::string &);
-        bool is_vars(std::string, std::string&); 
+        bool is_vars(std::string, std::string&);
         void operate_code_file();
+        void change_var(std::string);
 
         //get_data operates
         int get_data(int);
@@ -36,8 +55,19 @@ class CompileManage
         void get_uint_from_usect(int);
         void get_vars(int);
         std::string get_var(int);
+        int get_int_from_string(std::string, int&);
+        unsigned long get_uint_from_string(std::string, int&);
 
+        // operate text;
+        int get_text(int );
+        std::string get_label(std::string);
+        std::string get_instruction(std::string);
+        std::string get_arg1(std::string);
+        std::string get_arg2(std::string);
+        void add_nop_jmp();
+        void add_nop_conflict(); 
         //print test
+        void print_instructions_record();
         void print_labels_record();
         void print_vars_record();
         void print_binary_file();
